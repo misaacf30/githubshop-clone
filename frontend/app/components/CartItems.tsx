b02443fe6021be650c0612fdbf4fe12bccc69acf
Miguel Fernandez <misaacf30@gmail.com>
2025-01-20 23:55:40 -0500

    update CartItems and cartSlice

--- frontend/app/components/CartItems.tsx
+++ frontend/app/components/CartItems.tsx
@@ -2,96 +2,109 @@
 
 import type { RootState } from "../lib/store/store"
 import { useDispatch, useSelector } from "react-redux"
-import { updateQuantity, removeFromCart, clearCart } from "../lib/store/features/cart/cartSlice"
+import { incrementQuantity, decrementQuantity, removeFromCart, clearCart } from "../lib/store/features/cart/cartSlice"
 import Image from "next/image"
 import Link from "next/link"
 
 export const CartItems = () => {
-  const cart = useSelector((state: RootState) => state.cartReducer.items)
+  const cartReducer = useSelector((state: RootState) => state.cartReducer);
+  const cart = cartReducer.items;
+  const cartSize = cartReducer.size;
   const dispatch = useDispatch()
 
   const handleIncrement = (item: any) => {
     if (item.quantity >= 1 && item.quantity < 10)
-      dispatch(updateQuantity({ id: item.id, quantity: item.quantity + 1 }))
+      dispatch(incrementQuantity({ id: item.id, size: item.size }))
   }
 
   const handleDecrement = (item: any) => {
-    if (item.quantity > 1)
-      dispatch(updateQuantity({ id: item.id, quantity: item.quantity - 1 }))
-    else if (item.quantity === 1)
-      dispatch(removeFromCart({ id: item.id, size: item.size }))
+    //if (item.quantity > 1)
+      dispatch(decrementQuantity({ id: item.id, size: item.size }))
+    //else if (item.quantity === 1)
+    //  dispatch(removeFromCart({ id: item.id, size: item.size}))
+  }
+
+  const remove = (item: any) => {
+    dispatch(removeFromCart({ id: item.id, size: item.size}))
   }
 
   return (
-    <ul className="flex flex-col w-full divide-y-[0.5px]">
-      {cart.map((item: any, index: number) =>
-        <li key={index} className="flex flex-col break-words w-full px-[16px] min-[868px]:px-[24px] py-[16px]">
-          <div className="flex w-full">
-            <div className="w-1/5">
-              <Link href={''}>
-                <Image
-                  src={item.image}
-                  alt={''}
-                  width={180}
-                  height={180}
-                  objectFit="contain"
-                  objectPosition="center"
-                  className="w-full"
-                />
-              </Link>
-            </div>
-
-            <div className="w-60 block">
-              <Link href={''}>
-                {item.name}
-              </Link>
-              <div>
-                Size: {item.size}
+    <div className="flex flex-col w-full">
+      <div className="w-full font-medium px-[16px] min-[868px]:px-[24px] py-[8px]">
+        {cartSize} items
+      </div>
+      <ul className="w-full flex flex-col divide-y-[0.5px] divide-dashed">
+        {cart.map((item: any, index: number) =>
+          <li key={index} className="flex flex-col w-full px-[16px] min-[868px]:px-[24px] py-[16px]">
+            <div className="flex w-full">
+              <div className="w-1/6 px-[8px]">
+                <Link href={''}>
+                  <Image
+                    src={item.image}
+                    alt={''}
+                    width={180}
+                    height={180}
+                    objectFit="contain"
+                    objectPosition="center"
+                    className="w-full"
+                  />
+                </Link>
               </div>
-              <div>
-                Price: ${item.price.toFixed(2)}/ea
+
+              <div className="w-4/6">
+                <Link
+                  href={''}
+                  className="text-[16px] text-[#272727] my-[8px] block "
+                >
+                  {item.name}
+                </Link>
+                {/* {item.size && ( */}
+                  <div className="text-[14px] text-[#888888]  py-[2px]">
+                    Size: {item.size}
+                  </div>
+                {/* )} */}
+                <div className="text-[14px] text-[#888888] py-[2px]">
+                  Price: ${item.price.toFixed(2)}/ea
+                </div>
+              </div>
+
+              <div className="w-1/6 ml-auto">
+                <div className="text-[16px] text-right text-[#373737] font-semibold">
+                  ${(item.price * item.quantity).toFixed(2)}
+                </div>
               </div>
             </div>
 
-            <div className="w-1/5 ml-auto">
-              <div className="text-right">
-                ${(item.price * item.quantity).toFixed(2)}
+            <div className="flex items-center justify-end">
+              <div className="mr-[28px]">
+                <button
+                  onClick={() => remove(item)}
+                  className="underline text-[14px] text-[#8A8A8A]"
+                >
+                  Remove
+                </button>
+              </div>
+              <div className="border rounded-lg flex items-center justify-center p-[4px]">
+                <button
+                  onClick={() => handleDecrement(item)}
+                  className='text-[18px] text-[#8A8A8A] font-extrabold px-[4px]'>
+                  -
+                </button>
+                <span className="text-[14px] box-border flex mx-[28px] items-center">
+                  {item.quantity}
+                </span>
+                <button
+                  onClick={() => handleIncrement(item)}
+                  className='text-[18x] text-[#8A8A8A] font-extrabold px-[4px]'>
+                  +
+                </button>
               </div>
             </div>
-          </div>
 
-          <div className="flex items-center justify-end">
-            <div className="mr-[28px]">
-              <button
-                onClick={() => dispatch(removeFromCart({ id: item.id, size: item.size }))}
-                className="underline"
-              >
-                Remove
-              </button>
-            </div>
-            <div>
-              <button
-                onClick={() => handleIncrement(item)}
-                className='text-[18px] text-[#8A8A8A] font-extrabold px-[8px] py-[7px]'>
-                -
-              </button>
-              <input
-                type="number"
-                id=""
-                value={item.quantity}
-                onChange={() => console.log()}
-                className='[&::-webkit-inner-spin-button]:appearance-none text-[14px] text-center w-[48px] h-[48px] px-[6px] py-[14px]'
-              />
-              <button
-                onClick={() => handleDecrement(item)}
-                className='text-[18x] text-[#8A8A8A] font-extrabold px-[8px] py-[7px]'>
-                +
-              </button>
-            </div>
-          </div>
+          </li>
+        )}
+      </ul>
 
-        </li>
-      )}
-    </ul>
+    </div>
   )
 }
--- frontend/app/lib/store/features/cart/cartSlice.ts
+++ frontend/app/lib/store/features/cart/cartSlice.ts
@@ -11,10 +11,14 @@ interface CartItem {
 
 interface CartState {
     items: CartItem[],
+    size: 0,
+    total: 0,
 }
 
 const initialState: CartState = {
     items: [],
+    size: 0,
+    total: 0,
 }
 
 export const cartSlice = createSlice({
@@ -27,21 +31,50 @@ export const cartSlice = createSlice({
                 existingItem.quantity += action.payload.quantity;
             else
                 state.items.push(action.payload);
+            state.size += action.payload.quantity;
+            state.total += action.payload.price * action.payload.quantity;
         },
-        removeFromCart: (state, action: PayloadAction<{ id: string, size: string }>) => {
-            state.items = state.items.filter((item) => item.id === action.payload.id && item.size !== action.payload.size);
+        removeFromCart: (state, action: PayloadAction<{ id: string, size: string}>) => {
+            const existingItem = state.items.find((item) => item.id === action.payload.id && item.size === action.payload.size);        
+            if(existingItem) {  
+                const quantity = existingItem.quantity; 
+                const price = existingItem.price;        
+                state.items = state.items.filter((item) => item != existingItem);
+                state.size -= quantity;
+                state.total -= price * quantity;
+            }      
         },
-        updateQuantity: (state, action: PayloadAction<{ id: string, quantity: number }>) => {
-            const item = state.items.find((item) => item.id === action.payload.id);
-            if (item)
-                item.quantity = action.payload.quantity;
+        incrementQuantity: (state, action: PayloadAction<{ id: string, size: string }>) => {
+            const existingItem = state.items.find((item) => item.id === action.payload.id && item.size === action.payload.size);
+            if (existingItem) {
+                existingItem.quantity++;
+                state.size++;
+                state.total += existingItem.price;
+            }
+
+        },
+        decrementQuantity: (state, action: PayloadAction<{ id: string, size: string }>) => {
+            const existingItem = state.items.find((item) => item.id === action.payload.id && item.size === action.payload.size);
+            if (existingItem && existingItem.quantity > 1) {
+                existingItem.quantity--;
+                state.size--;
+                state.total -= existingItem.price;
+            }
+            else if(existingItem && existingItem.quantity === 1) {
+                const price = existingItem.price;
+                state.items = state.items.filter((item) => item != existingItem);
+                state.size--;
+                state.total -= price;              
+            }
         },
         clearCart(state) {
             state.items = [];
+            state.size = 0;
+            state.total = 0;
         },
     },
 })
 
-export const { addToCart, removeFromCart, updateQuantity, clearCart } = cartSlice.actions
+export const { addToCart, removeFromCart, incrementQuantity, decrementQuantity, clearCart } = cartSlice.actions
 
 export default cartSlice.reducer
